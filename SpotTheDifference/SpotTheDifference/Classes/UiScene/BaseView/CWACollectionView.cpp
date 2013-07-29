dada13a8e86fe5fec90f58359ac05e4692185236
allenhmo1254 <allenhmo1254@sina.com>
2013-07-29 23:56:42 +0800

    fdsfa

---
+++ SpotTheDifference/SpotTheDifference/Classes/UiScene/BaseView/CWACollectionView.cpp
@@ -0,0 +1,38 @@
+//
+//  CWACollectionView.cpp
+//  SpotTheDifference
+//
+//  Created by 景 中杰 on 13-7-29.
+//
+//
+
+#include "CWACollectionView.h"
+
+using namespace cocos2d;
+
+
+//CWACollectionView* CWACollectionView::create(CCSize collectionSize, CCSize collectionDistance)
+//{
+//    CWACollectionView *view = new CWACollectionView();
+//    if (view && view -> init()) {
+//        view -> autorelease();
+//        view -> setCollectionSize(collectionSize);
+//        view -> setCollectionDistance(collectionDistance);
+//        return view;
+//    } else {
+//        delete view;
+//        view = NULL;
+//        return NULL;
+//    }
+//}
+
+
+
+
+
+
+
+//bool init()
+//{
+//
+//}
---
+++ SpotTheDifference/SpotTheDifference/Classes/UiScene/BaseView/CWACollectionView.h
@@ -0,0 +1,25 @@
+//
+//  CWACollectionView.h
+//  SpotTheDifference
+//
+//  Created by 景 中杰 on 13-7-29.
+//
+//
+
+#ifndef __SpotTheDifference__CWACollectionView__
+#define __SpotTheDifference__CWACollectionView__
+
+#include <iostream>
+#include "CWAScrollView.h"
+
+class CWACollectionView : public CWAScrollView {
+//protected:
+//    CC_SYNTHESIZE(cocos2d::CCSize, collectionSize_, CollectionSize);              //按钮的横向和纵向的个数
+//    CC_SYNTHESIZE(cocos2d::CCSize, collectionDistance_, CollectionDistance);      //按钮之间的距离
+//public:
+//    static CWACollectionView* create(cocos2d::CCSize collectionSize, cocos2d::CCSize collectionDistance, cocos2d::CCNode *node);
+//    
+//    virtual bool init();
+};
+
+#endif /* defined(__SpotTheDifference__CWACollectionView__) */
